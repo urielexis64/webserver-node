97a3cbc1d85e306cf6cad8ba143ef895df9a61d1
Uriel Aispuro <urielalexis64@gmail.com>
2021-04-01 17:50:55 -0700

    server using express

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
-const http = require("http");
+const express = require("express");
+const app = express();
+const port = 4200;
+const hostname = "192.168.3.15";
 
-http.createServer((req, res) => {
-	res.write("Hello world");
+app.set("view engine", "hbs");
 
-	res.end();
-}).listen(8080);
+// Static content
+app.use(express.static("public"));
 
-console.log("Listening on port: 8080");
+app.get("/", (req, res) => {
+	res.render("home");
+});
+
+app.get("/generic", (req, res) => {
+	res.sendFile(__dirname + "/public/generic.html");
+});
+
+app.get("/elements", (req, res) => {
+	res.sendFile(__dirname + "/public/elements.html");
+});
+
+app.get("*", (req, res) => {
+	res.sendFile(__dirname + "/public/404.html");
+});
+
+app.listen(port, hostname, () => {
+	console.log(`Example app listening at http://${hostname}:${port}`);
+});
